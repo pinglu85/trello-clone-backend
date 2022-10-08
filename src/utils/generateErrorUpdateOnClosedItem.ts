a28a1aa1d98ac4eaff900d1184456c7bafda243a
pinglu85 <luping8576@hotmail.com>
2022-10-08 07:04:12 +0200

    feat(graphql): make error message more specific when item cannot be found

--- src/utils/generateErrorUpdateOnClosedItem.ts
+++
@@ -1,9 +0,0 @@
-import { UserInputError } from 'apollo-server-express';
-
-function generateErrorUpdateOnClosedItem(itemName: string): UserInputError {
-  return new UserInputError(
-    `Cannot update a ${itemName} that is already archived`
-  );
-}
-
-export default generateErrorUpdateOnClosedItem;
