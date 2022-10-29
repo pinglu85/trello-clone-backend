3198f80189e6e3aac981c807f358e5d7ffbb4231
pinglu85 <luping8576@hotmail.com>
2022-10-29 15:52:24 +0200

    chore(isErrorDuplicateRank): update pg constraint name

--- src/models/utils/isErrorDuplicateRank.ts
+++ src/models/utils/isErrorDuplicateRank.ts
@@ -6,7 +6,7 @@ function isErrorDuplicateRank(error: unknown): boolean {
   return (
     error instanceof DatabaseError &&
     error.code === PG_UNIQUE_VIOLATION &&
-    error.constraint === 'lists_uc_board_id_rank'
+    error.constraint === 'lists_board_id_rank_closed_key'
   );
 }
 
