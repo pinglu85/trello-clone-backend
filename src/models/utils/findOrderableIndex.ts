fc23b5014577da16c3dbcc87582d2d5cda7500a2
pinglu85 <luping8576@hotmail.com>
2022-10-29 16:14:59 +0200

    refactor(findOrderableIndex): rename `items` to `sortedOrderables`

--- src/models/utils/findOrderableIndex.ts
+++ src/models/utils/findOrderableIndex.ts
@@ -1,12 +1,15 @@
 import type { Orderable } from '../types';
 
-function findOrderableIndex(items: Orderable[], targetRank: string): number {
+function findOrderableIndex(
+  sortedOrderables: Orderable[],
+  targetRank: string
+): number {
   let left = 0;
-  let right = items.length - 1;
+  let right = sortedOrderables.length - 1;
 
   while (left <= right) {
     const mid = left + Math.floor((right - left) / 2);
-    const midItemRank = items[mid].rank;
+    const midItemRank = sortedOrderables[mid].rank;
 
     if (midItemRank === targetRank) return mid;
 
