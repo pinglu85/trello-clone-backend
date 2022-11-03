246c9e6ecea522710e6d2f57ec6913e77239e9c5
pinglu85 <luping8576@hotmail.com>
2022-11-03 19:41:35 +0100

    update

--- src/models/utils/isErrorDuplicateRank.ts
+++ src/models/utils/isErrorDuplicateRank.ts
@@ -5,8 +5,8 @@ import { PG_UNIQUE_VIOLATION } from '../constants/postgresErrorCodes';
 function isErrorDuplicateRank(error: unknown): boolean {
   if (error instanceof DatabaseError && error.code === PG_UNIQUE_VIOLATION) {
     return (
-      error.constraint === 'lists_board_id_rank_closed_key' ||
-      error.constraint === 'cards_list_id_rank_closed_key'
+      error.constraint === 'lists_board_id_rank_idx' ||
+      error.constraint === 'cards_list_id_rank_idx'
     );
   }
 
